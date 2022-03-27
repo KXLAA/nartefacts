f89589b882baf8a0f507f1233d44074965ef65b7
KXLAA <kolade.afode@yahoo.com>
2022-03-27 18:58:14 +0100

    refator: add prisma client to global object
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,21 @@
 import { PrismaClient } from '@prisma/client'
 
-// Prevent multiple instances of Prisma Client in development
-declare const global: NodeJS.Global & { prisma?: PrismaClient }
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-export const prisma = global.prisma || new PrismaClient()
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
