aea39d0ed2a3169b03088d495a75e1d6054d5a45
KXLAA <kolade.afode@yahoo.com>
2022-04-02 14:10:46 +0100

    fix: remove global setup in test

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -3,8 +3,8 @@ import 'jest-styled-components'
 
 //https://nextjs.org/docs/basic-features/environment-variables#test-environment-variables
 //To be able to load .env files for tests
-import { loadEnvConfig } from '@next/env'
-export default async () => {
-  const projectDir = process.cwd()
-  loadEnvConfig(projectDir)
-}
+// import { loadEnvConfig } from '@next/env'
+// export default async () => {
+//   const projectDir = process.cwd()
+//   loadEnvConfig(projectDir)
+// }
