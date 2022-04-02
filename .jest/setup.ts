7823107642c0a1f6f66874b32f5824dd875c98e7
KXLAA <kolade.afode@yahoo.com>
2022-04-02 13:19:34 +0100

    feat: update jest setup to allow next to load env files from /env.test

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,2 +1,10 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+
+//https://nextjs.org/docs/basic-features/environment-variables#test-environment-variables
+//To be able to load .env files for tests
+import { loadEnvConfig } from '@next/env'
+export default async () => {
+  const projectDir = process.cwd()
+  loadEnvConfig(projectDir)
+}
--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,6 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  //https://github.com/vercel/next.js/discussions/16270
+  globalSetup: '/.jest/setup.ts',
 }
