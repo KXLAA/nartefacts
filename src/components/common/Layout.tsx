566a4dda90739b472d26c3c35b5b91cc1b3e1eb5
KXLAA <kolade.afode@yahoo.com>
2022-04-09 19:21:56 +0100

    feat: update home page with common layout
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -5,10 +5,9 @@ const LayoutStyled = styled.main`
   max-width: 1248px;
   margin: 0 auto;
   padding: 1.5rem;
+  padding-top: 5rem;
 `
 
-export const Layout = ({
-  children,
-}: React.PropsWithChildren<Record<never, unknown>>) => {
+export const Layout: React.FC = ({ children }) => {
   return <LayoutStyled>{children}</LayoutStyled>
 }
