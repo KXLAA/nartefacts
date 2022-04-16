965e783e86b52a46cfbbcfa2b244ab0484f427e5
KXLAA <kolade.afode@yahoo.com>
2022-04-16 13:45:52 +0100

    feat: update header tests
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -1,4 +1,6 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { createMockRouter } from 'utils'
+import { RouterContext } from 'next/dist/shared/lib/router-context'
 
 import { Header } from '..'
 
@@ -20,8 +22,45 @@ describe('<Header />', () => {
 
   it('should render the navigation buttons', () => {
     const { getByText } = render(<Header />)
-    expect(getByText(/switch/i)).toBeInTheDocument()
+    expect(getByText(/info/i)).toBeInTheDocument()
     expect(getByText(/likes/i)).toBeInTheDocument()
     expect(getByText(/create/i)).toBeInTheDocument()
+  })
+
+  it('has navigation button links with the correct href attributes', () => {
+    render(
+      <RouterContext.Provider
+        value={createMockRouter({ query: { id: '33' }, pathname: 'kola' })}
+      >
+        <Header />
+      </RouterContext.Provider>,
+    )
+
+    // /info
+    expect(screen.getByText(/info/i)).toHaveAttribute('href', '/info')
+    // /likes
+    expect(screen.getByText(/likes/i)).toHaveAttribute('href', '/likes')
+    // /likes
+    expect(screen.getByText(/create/i)).toHaveAttribute('href', '/create')
+  })
+
+  it('has navigation buttons that work', () => {
+    const router = createMockRouter({
+      query: { id: '33' },
+      pathname: 'kola',
+    })
+
+    render(
+      <RouterContext.Provider value={router}>
+        <Header />
+      </RouterContext.Provider>,
+    )
+
+    fireEvent.click(screen.getByRole('link', { name: /info/i }))
+    expect(router.push).toHaveBeenCalledWith('/info', '/info', {
+      locale: undefined,
+      scroll: undefined,
+      shallow: undefined,
+    })
   })
 })
