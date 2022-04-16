1b7270a06778ed53f56cae5690ee7b20526cd460
KXLAA <kolade.afode@yahoo.com>
2022-04-16 13:43:49 +0100

    feat: add new ButtonLink component
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,13 @@
 import * as S from './styles'
 import { capitalize } from 'utils'
+import React, { forwardRef, ForwardRefRenderFunction } from 'react'
 
 export type ButtonProps = {
   text?: string
   buttonColor?: string
   textColor?: string
+  onClick?: () => void
+  href?: string
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -18,3 +21,22 @@ export const Button: React.FC<ButtonProps> = ({
     </S.Button>
   )
 }
+
+const Link: ForwardRefRenderFunction<HTMLAnchorElement, ButtonProps> = (
+  { buttonColor, text, textColor, onClick, href },
+  ref,
+) => {
+  return (
+    <S.Link
+      buttonColor={buttonColor}
+      textColor={textColor}
+      href={href}
+      onClick={onClick}
+      ref={ref}
+    >
+      {capitalize(text!) || 'ButtonLink'}
+    </S.Link>
+  )
+}
+
+export const ButtonLink = forwardRef(Link)
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -30,3 +30,28 @@ export const Button = styled.button<StyledButtonProps>`
     transform: translateX(0rem) translateY(0.125rem);
   }
 `
+
+export const Link = styled.a<StyledButtonProps>`
+  display: flex;
+  max-width: 301px;
+  width: 100%;
+  height: 82px;
+  align-items: center;
+  justify-content: center;
+  padding: 16px 20px;
+  background-color: ${({ buttonColor }) => buttonColor || grayPrimary};
+  font-size: 36px;
+  border: none;
+  border-radius: 8px;
+  color: ${({ textColor }) => textColor || blackSecondary};
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: #dddddd;
+  }
+
+  &:active {
+    transform: translateX(0rem) translateY(0.125rem);
+  }
+`
