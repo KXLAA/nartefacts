72424449e5ebc30f3898264f4fc84badb115bb47
KXLAA <kolade.afode@yahoo.com>
2022-04-16 13:44:13 +0100

    feat: update button tests
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -2,8 +2,10 @@ import { render, screen } from '@testing-library/react'
 import { colors, testColors } from 'styles/global'
 const { grayPrimary, blackSecondary } = colors
 const { black, white } = testColors
-
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { createMockRouter } from 'utils'
 import { Button } from '..'
+import { ButtonLink } from '..'
 
 describe('<Button />', () => {
   it('should render the button with default values & styles', () => {
@@ -34,5 +36,17 @@ describe('<Button />', () => {
       'background-color': black,
       color: white,
     })
+  })
+
+  it('Button Link should render as an <a> tag with href attribute', () => {
+    render(
+      <RouterContext.Provider
+        value={createMockRouter({ query: { id: '33' }, pathname: 'kola' })}
+      >
+        <ButtonLink href="/test" />
+      </RouterContext.Provider>,
+    )
+    expect(screen.getByRole('link', { name: /button/i })).toBeInTheDocument()
+    expect(screen.getByRole('link')).toHaveAttribute('href', '/test')
   })
 })
