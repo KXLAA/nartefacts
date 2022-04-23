6443fdd5eadcb9a196239605df56dcf34da6cabb
KXLAA <kolade.afode@yahoo.com>
2022-04-23 09:19:56 +0100

    feat: add some tests to card component
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/Card/__tests__/Card.test.tsx
+++ src/components/Card/__tests__/Card.test.tsx
@@ -1,11 +1,20 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import { Card } from '..'
 
-describe('<Card />', () => {
-  it('should render the heading', () => {
-    render(<Card />)
+const albumArt =
+  'https://ucarecdn.com/c515e4b5-a5bb-44a3-85d0-f139497e0de0/forbrokenearsep_tems.jpeg'
 
-    expect(screen.getByRole('heading', { name: /Card/i })).toBeInTheDocument()
+describe('<Card />', () => {
+  it('should render card with image', () => {
+    const { getByAltText } = render(<Card albumArt={albumArt} />)
+    const image = getByAltText('album art')
+    expect(image).toBeInTheDocument()
+    expect(image).toBeTruthy()
+  })
+
+  it('should render card  correctly', () => {
+    const { container } = render(<Card albumArt={albumArt} />)
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
