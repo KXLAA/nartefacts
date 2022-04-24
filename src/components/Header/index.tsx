fd8b270011dc1d3bdc80c0fc03c1d6b9693d966c
KXLAA <kolade.afode@yahoo.com>
2022-04-24 17:32:54 +0100

    feat: extend header component
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,57 @@
+import Link from 'next/link'
 import * as S from './styles'
 import { Logo } from 'components/Logo'
 import { ButtonLink } from 'components/Button'
 import { colors } from 'styles/global'
-import Link from 'next/link'
+import { Arrow } from 'components/Icons'
 
-export const Header = () => {
+export type HeaderProps = {
+  primary?: boolean
+  secondary?: boolean
+}
+
+export const Header: React.FC<HeaderProps> = ({ primary, secondary }) => {
   return (
-    <S.Layout>
-      <Logo />
+    <>
+      {primary ? (
+        <S.Layout>
+          <Logo />
 
-      <S.Nav>
-        <Link href="/create" passHref>
-          <ButtonLink text="create" />
-        </Link>
+          <S.Nav>
+            <Link href="/create" passHref>
+              <ButtonLink text="create" />
+            </Link>
 
-        <Link href="/likes" passHref>
-          <ButtonLink
-            text="likes"
-            buttonColor={colors.blackSecondary}
-            textColor={colors.grayPrimary}
-          />
-        </Link>
+            <Link href="/likes" passHref>
+              <ButtonLink
+                text="likes"
+                buttonColor={colors.blackSecondary}
+                textColor={colors.grayPrimary}
+              />
+            </Link>
 
-        <Link href="/info" passHref>
-          <ButtonLink text="info" />
-        </Link>
-      </S.Nav>
-    </S.Layout>
+            <Link href="/info" passHref>
+              <ButtonLink text="info" />
+            </Link>
+          </S.Nav>
+        </S.Layout>
+      ) : null}
+
+      {secondary ? (
+        <S.Layout>
+          <S.Nav>
+            <Link href="/" passHref>
+              <Arrow position="left" color={colors.grayPrimary} size={48} />
+            </Link>
+
+            <Link href="/">
+              <Logo width="250" />
+            </Link>
+
+            <S.Spacer />
+          </S.Nav>
+        </S.Layout>
+      ) : null}
+    </>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,10 +4,17 @@ export const Layout = styled.header`
   display: flex;
   flex-direction: column;
   gap: 3rem;
+  width: 100%;
 `
 
 export const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
-  width: 100%;
+  align-items: center;
+  justify-items: center;
+`
+
+export const Spacer = styled.div`
+  width: 48px;
+  height: 48px;
 `
