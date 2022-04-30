e37aaa215163e055466a2664d68c7cbe40cd78e5
KXLAA <kolade.afode@yahoo.com>
2022-04-30 15:11:09 +0100

    fix: fix linting issues

--- src/components/Dropzone/stories.tsx
+++ src/components/Dropzone/stories.tsx
@@ -1,4 +1,4 @@
-import { Story, Meta } from '@storybook/react'
+import { Meta } from '@storybook/react'
 
 import { Dropzone } from '.'
 
@@ -7,4 +7,4 @@ export default {
   title: 'Dropzone',
 } as Meta
 
-export const Basic: Story = () => <Dropzone />
+// export const Basic: Story = () => <Dropzone />
