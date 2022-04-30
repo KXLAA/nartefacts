0b1df6fe4b33cc9dfb5ea9caf9bfcc3944596318
KXLAA <kolade.afode@yahoo.com>
2022-04-30 14:01:34 +0100

    feat: add upload to s3 & some loading states
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/Dropzone/index.tsx
+++ src/components/Dropzone/index.tsx
@@ -1,15 +1,38 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import * as S from './styles'
 import { UploadPlus } from 'components/Icons'
+import { useS3Upload } from 'next-s3-upload'
+import { useGenerateColorsMutation } from '../../../graphql/generated/graphql'
+import { RotatingLines } from 'react-loader-spinner'
 
 export const Dropzone = () => {
-  const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
-    console.log(acceptedFiles)
+  const { uploadToS3 } = useS3Upload()
+  const [imageUrl, setImageUrl] = useState<undefined | string>()
+  const [loading, setLoading] = useState<boolean>()
+  const [colors, setColors] = useState<string[] | null | undefined>([])
+  const [generateColors] = useGenerateColorsMutation()
+
+  const onDrop = useCallback(async (acceptedFiles) => {
+    setLoading(true)
+    const { url } = await uploadToS3(acceptedFiles[0])
+    setImageUrl(url)
+    if (url) {
+      setLoading(false)
+      const { data } = await generateColors({
+        variables: {
+          imageUrl: url,
+        },
+      })
+      setColors(data?.generateColors?.colors)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
+    maxFiles: 1,
+    accept: 'image/jpeg,image/png',
   })
 
   return (
@@ -19,12 +42,18 @@ export const Dropzone = () => {
         <h1>Dropzone</h1>
 
         <S.Content>
-          <UploadPlus size={72} strokeWidth={0.5} color="#5A5A5A" />
-
-          {isDragActive ? (
-            <p>drop the files here ...</p>
+          {loading ? (
+            <RotatingLines width="100" strokeColor="#FF5733" />
           ) : (
-            <p>drag {'n'} drop your image here, or click to select files</p>
+            <>
+              <UploadPlus size={72} strokeWidth={0.5} color="#5A5A5A" />
+
+              {isDragActive ? (
+                <p>drop the files here ...</p>
+              ) : (
+                <p>drag {'n'} drop your image here, or click to select files</p>
+              )}
+            </>
           )}
         </S.Content>
       </S.Drop>
