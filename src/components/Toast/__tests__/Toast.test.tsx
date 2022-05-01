6af4e034a51dd5da1bc8d5fac698a119bfd35222
KXLAA <kolade.afode@yahoo.com>
2022-05-01 20:03:28 +0100

    fix: fix some test cases in toast component
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/Toast/__tests__/Toast.test.tsx
+++ src/components/Toast/__tests__/Toast.test.tsx
@@ -1,12 +1,20 @@
 import { render } from '@testing-library/react'
+import { imageUrl } from 'components/utils'
 
 import { Toast } from '..'
 
 describe('<Toast />', () => {
-  it('should render the heading', () => {
-    const { getByRole } = render(<Toast />)
-    const heading = getByRole('heading', { name: /Toast/i })
+  it('should render the toast component', () => {
+    const { getByAltText } = render(<Toast imageUrl={imageUrl} />)
+    const image = getByAltText(
+      /image uploaded by a user to generate color palettes/i,
+    )
+    expect(image).toBeInTheDocument()
+    expect(image).toBeTruthy()
+  })
 
-    expect(heading).toBeInTheDocument()
+  it('should render the component correctly', () => {
+    const { container } = render(<Toast imageUrl={imageUrl} />)
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
