e9f485c1aaa4637358a333b476295f8eeef9c818
KXLAA <kolade.afode@yahoo.com>
2022-07-31 11:43:16 +0100

    fix: remove redundant icon folder
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/components/icons/Check.tsx
+++
@@ -1,7 +0,0 @@
-import { CheckCircle2 as Icon } from 'lucide-react'
-
-import { IconProps } from '@/components/icons'
-
-export const Check = ({ color, size, strokeWidth }: IconProps) => {
-  return <Icon color={color} size={size} strokeWidth={strokeWidth} />
-}
--- src/components/icons/Copy.tsx
+++
@@ -1,7 +0,0 @@
-import { Copy as Icon } from 'lucide-react'
-
-import { IconProps } from '@/components/icons'
-
-export const Copy = ({ color, size, strokeWidth }: IconProps) => {
-  return <Icon color={color} size={size} strokeWidth={strokeWidth} />
-}
