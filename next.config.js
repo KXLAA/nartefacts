caf6fa376d71fe51c2e0b8cf814909d1732f3a98
KXLAA <kolade.afode@yahoo.com>
2022-07-31 20:36:22 +0100

    fix: add bundle anlyser plugin
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa')
+
 const isProd = process.env.NODE_ENV === 'production'
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 module.exports = withPWA({
   pwa: {
@@ -11,3 +15,5 @@ module.exports = withPWA({
     domains: ['ucarecdn.com', 'nartefact-upload.s3.eu-west-2.amazonaws.com'],
   },
 })
+
+module.exports = withBundleAnalyzer({})
