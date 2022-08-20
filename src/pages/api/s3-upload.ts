2a0b35c3fb30cb983c1e07f75937b2437ab1917b
KXLAA <kolade.afode@yahoo.com>
2022-08-20 15:55:17 +0100

    fix: fix upload urls in dev and prod
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- src/pages/api/s3-upload.ts
+++ src/pages/api/s3-upload.ts
@@ -1,1 +1,9 @@
-export { APIRoute as default } from 'next-s3-upload'
+import { APIRoute } from 'next-s3-upload'
+
+export default APIRoute.configure({
+  key(req, filename) {
+    return process.env.NODE_ENV === 'development'
+      ? `narefacts-dev-uploads/images/${filename}`
+      : `narefacts-uploads/images/${filename}`
+  },
+})
