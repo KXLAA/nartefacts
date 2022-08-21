ab34964235c3340a1f21bca8dd32e41d49bc85fd
KXLAA <kolade.afode@yahoo.com>
2022-08-21 09:53:40 +0100

    ci: still trying to fix
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'cypress'
 
-const uri =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000/api/graphql'
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
+// const uri =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://localhost:3000/api/graphql'
+//     : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
 
 export default defineConfig({
   chromeWebSecurity: false,
