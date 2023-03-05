de489427bd5c49bb198671b3acec876629a74d64
KXLAA <kolade.afode@yahoo.com>
2023-03-05 16:30:41 +0000

    feat: more work on create page
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- components/create/ImagePreview.tsx
+++ components/create/ImagePreview.tsx
@@ -1,3 +1,4 @@
+import copy from "copy-to-clipboard";
 import { motion } from "framer-motion";
 import { XCircle } from "lucide-react";
 import Image from "next/image";
@@ -5,7 +6,7 @@ import Image from "next/image";
 import { CH } from "@/lib/color-helpers";
 import { useMouseOver } from "@/lib/hooks/use-mouse-over";
 
-import type { CreateController } from "./controllers/imagePreviewController";
+import type { CreateController } from "./controller";
 
 interface ImagePreviewProps extends CreateController {
   className?: string;
@@ -13,26 +14,20 @@ interface ImagePreviewProps extends CreateController {
 
 export function ImagePreview(props: ImagePreviewProps) {
   return props.isUploaded && props?.palette ? (
-    <div>
-      <div className="flex gap-4">
-        <button>SAVE</button>
-        <button>UPLOAD NEW</button>
-        <button>EXPORT</button>
+    <div className="grid w-full h-full grid-cols-2 gap-2 p-6 rounded-md shadow bg-cod-gray-800 ">
+      <div className="flex flex-col items-center gap-4">
+        <Image
+          src={props.palette?.imageUrl || "/images/placeholder.png"}
+          height={1000}
+          width={1000}
+          alt={"user uploaded image"}
+          placeholder="blur"
+          blurDataURL={props.palette?.imageUrl || "/images/placeholder.png"}
+          className="rounded"
+        />
       </div>
 
-      <div className="flex items-start justify-between w-full gap-8 p-6 rounded-md shadow bg-cod-gray-800">
-        <div className="flex flex-col items-center gap-4">
-          <Image
-            src={props.palette?.imageUrl || "/images/placeholder.png"}
-            height={1000}
-            width={1000}
-            alt={"user uploaded image"}
-            placeholder="blur"
-            blurDataURL={props.palette?.imageUrl || "/images/placeholder.png"}
-            className="rounded"
-          />
-          <p className="text-xl">{props.palette.title}</p>
-        </div>
+      <div className="flex flex-col justify-between w-full h-full gap-2">
         {props.palette.palette && (
           <div className="grid w-full grid-cols-4 gap-1">
             {props.palette.palette.map((color) => (
@@ -40,6 +35,25 @@ export function ImagePreview(props: ImagePreviewProps) {
             ))}
           </div>
         )}
+
+        <div className="self-center p-3 px-6 text-center rounded bg-cod-gray-500 shadow-border-shiny">
+          <p className="text-xl font-semibold">{props.palette.title}</p>
+          <p className="text-sm text-silver-900 font-extralight">
+            Generated at 24:53 Monday{" "}
+          </p>
+        </div>
+
+        <div className="flex items-center self-end justify-end w-full gap-2">
+          <button className="w-full p-3 rounded bg-cod-gray-500 shadow-border-shiny">
+            Save
+          </button>
+          <button className="w-full p-3 rounded bg-cod-gray-500 shadow-border-shiny">
+            Reset
+          </button>
+          <button className="w-full p-3 rounded bg-cod-gray-500 shadow-border-shiny">
+            Export
+          </button>
+        </div>
       </div>
     </div>
   ) : null;
@@ -50,27 +64,38 @@ function ColorBox(props: { color: string } & CreateController) {
   const isValidHex = CH.isValidHexCode(props.color);
 
   return isValidHex ? (
-    <div
-      className="flex items-center justify-center w-full h-20 transition-all opacity-75 hover:opacity-100 aspect-square shadow-border-shiny"
-      style={{ backgroundColor: props.color }}
-      ref={hoverRef}
-    >
-      {isHovered && (
-        <motion.button
-          className="text-sm text-white"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          onClick={() => {
-            props.removeColor.mutateAsync({
-              color: props.color,
-              id: props.palette ? props.palette.id : "",
-            });
-          }}
-        >
-          <XCircle className="w-3 h-3" />
-        </motion.button>
-      )}
+    <div className="flex flex-col items-center justify-center p-2 rounded bg-cod-gray-500 shadow-border-shiny">
+      <div
+        className="flex items-center justify-center w-full h-20 transition-all rounded opacity-75 hover:opacity-100 aspect-square shadow-border-shiny"
+        style={{ backgroundColor: props.color }}
+        ref={hoverRef}
+      >
+        {isHovered && (
+          <motion.button
+            className="text-sm text-white"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={() => {
+              props.removeColor.mutateAsync({
+                color: props.color,
+                id: props.palette ? props.palette.id : "",
+              });
+            }}
+          >
+            <XCircle className="w-3 h-3" />
+          </motion.button>
+        )}
+      </div>
+      <motion.span
+        className="mt-1 text-sm font-semibold cursor-pointer text-silver"
+        onClick={() => {
+          copy(props.color);
+        }}
+        whileTap={{ scale: 0.95, color: props.color }}
+      >
+        {props.color}
+      </motion.span>
     </div>
   ) : (
     <div className="flex items-center justify-center w-full h-20 transition-all border border-dashed opacity-75 hover:opacity-100 aspect-square shadow-border-shin border-cod-gray-50" />
--- components/create/controller.ts
+++ components/create/controller.ts
@@ -4,14 +4,27 @@ import React from "react";
 import { useDropzone } from "react-dropzone";
 
 import { api } from "@/lib/api";
+import type { ColorsTuple } from "@/lib/color-helpers";
+import { useLocalStorage } from "@/lib/use-local-storage";
+
+type PreviewPage = "home" | "export" | "download";
+type SavedPalettes = palettes & {
+  savedAt: Date;
+};
 
 export function useCreate() {
   const [error, setError] = React.useState<string | null>(null);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [isUploaded, setIsUploaded] = React.useState<boolean>(false);
+  const [downloadUrl, setDownloadUrl] = React.useState<string | null>(null);
+  const [previewPage, setPreviewPage] = React.useState<PreviewPage>("home");
+  const [savedPallettes, setSavedPallettes] =
+    useLocalStorage<SavedPalettes | null>("nartefacts-pallettes", null);
+
   const [palette, setPalette] = React.useState<palettes>();
   const generateColors = api.palettes.generate.useMutation();
   const removeColor = api.palettes.removeColor.useMutation();
+  const exportColors = api.palettes.export.useMutation();
 
   const { uploadToS3, files } = useS3Upload();
   const onDrop = React.useCallback(async (acceptedFiles: File[]) => {
@@ -51,6 +64,38 @@ export function useCreate() {
     },
   });
 
+  async function exportAsCode(colors: ColorsTuple) {
+    const [data, error] = await exportColors.mutateAsync({
+      colors: colors,
+      type: "code",
+    });
+
+    if (error != null) return;
+    setDownloadUrl(data);
+    setPreviewPage("download");
+  }
+
+  async function exportAsCss(colors: ColorsTuple) {
+    const [data, error] = await exportColors.mutateAsync({
+      colors: colors,
+      type: "css",
+    });
+
+    if (error != null) return;
+
+    setDownloadUrl(data);
+    setPreviewPage("download");
+  }
+
+  function downloadExportedColors() {
+    if (downloadUrl == null) return;
+    window.open(downloadUrl);
+
+    setTimeout(() => {
+      setPreviewPage("home");
+    }, 5000);
+  }
+
   return {
     dropzone: {
       ...dropzone,
@@ -61,6 +106,24 @@ export function useCreate() {
     palette,
     isUploaded,
     removeColor,
+    exportColors,
+    export: {
+      asCode: exportAsCode,
+      asCss: exportAsCss,
+    },
+    downloadUrl,
+    downloadExportedColors,
+    page: {
+      current: previewPage,
+      home: previewPage === "home",
+      export: previewPage === "export",
+      download: previewPage === "download",
+      goToHome: () => setPreviewPage("home"),
+      goToExport: () => setPreviewPage("export"),
+      goToDownload: () => setPreviewPage("download"),
+    },
+    savedPallette: setSavedPallettes,
+    savedPallettes,
   };
 }
 
