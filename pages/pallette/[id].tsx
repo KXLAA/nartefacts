cb9d63904a7aa8c73e576919bec798a3ae3501b2
KXLAA <kolade.afode@yahoo.com>
2023-03-06 08:04:19 +0000

    fix: fix build issues
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- pages/pallette/[id].tsx
+++ pages/pallette/[id].tsx
@@ -1,9 +1,9 @@
-import { useCreate } from "@/components/create/controller";
+import { useCreatePage } from "@/components/create/controller";
 import { ImagePreview } from "@/components/create/ImagePreview";
 import { Layout } from "@/components/layout/Layout";
 
 export default function Create() {
-  const controller = useCreate();
+  const controller = useCreatePage();
 
   return (
     <Layout className="relative flex flex-col items-center justify-between w-full max-w-5xl min-h-screen p-2 m-auto overflow-hidden">
