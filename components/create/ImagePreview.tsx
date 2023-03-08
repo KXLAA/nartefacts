fb1e98ff1d1127c07864cbaad29a3e228afa8111
KXLAA <kolade.afode@yahoo.com>
2023-03-08 07:48:14 +0000

    fix: more clean ups
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- components/create/ImagePreview.tsx
+++ components/create/ImagePreview.tsx
@@ -1,6 +1,6 @@
 import copy from "copy-to-clipboard";
 import { motion } from "framer-motion";
-import { PlusCircle, XCircle } from "lucide-react";
+import { Edit, PlusCircle, Trash } from "lucide-react";
 import Image from "next/image";
 
 import { CH } from "@/lib/color-helpers";
@@ -22,58 +22,31 @@ export function ImagePreview(props: ImagePreviewProps) {
   return props.isUploaded && props?.palette ? <Preview {...props} /> : null;
 }
 
-function ColorBox(
-  props: {
-    color: string;
-    index: number;
-  } & CreateController
-) {
-  const isValidHex = CH.isValidHexCode(props.color);
-
-  return isValidHex ? (
-    <motion.div
-      className="flex flex-col items-center justify-center p-2 rounded bg-cod-gray-500 shadow-border-shiny"
-      style={{
-        ...props.colors.animation.getRandomTransformOrigin(),
-      }}
-      custom={props.index}
-      variants={props.colors.animation.variants}
-      animate={props.colors.animation.controls}
-    >
-      <div
-        className="flex items-center justify-center w-full h-20 transition-all rounded opacity-75 hover:opacity-100 aspect-square shadow-border-shiny"
-        style={{ backgroundColor: props.color }}
-      />
-      <motion.span
-        className="mt-1 text-sm font-semibold cursor-pointer text-silver"
-        onClick={() => {
-          copy(props.color);
-        }}
-        whileTap={{ scale: 0.95, color: props.color }}
-      >
-        {props.color}
-      </motion.span>
-    </motion.div>
-  ) : (
-    <button className="flex items-center justify-center w-full transition-all border border-dashed rounded opacity-75 hover:opacity-100 aspect-square shadow-border-shin border-cod-gray-50 h-[120px]">
-      <PlusCircle size={24} strokeWidth={1.22} />
-    </button>
-  );
-}
-
 function Home(props: ImagePreviewProps) {
-  return (
-    <div className="grid w-full h-full grid-cols-2 gap-2 p-6 rounded-md shadow bg-cod-gray-800 ">
-      <div className="flex flex-col items-center gap-4">
+  return props.palette?.imageUrl ? (
+    <motion.div
+      className="grid w-full h-full grid-cols-2 gap-2 p-6 rounded-md shadow bg-cod-gray-800 "
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="relative flex flex-col items-center gap-4">
         <Image
-          src={props.palette?.imageUrl || "/images/placeholder.png"}
-          height={1000}
-          width={1000}
+          src={props.palette?.imageUrl}
+          height={800}
+          width={800}
           alt={"user uploaded image"}
           placeholder="blur"
-          blurDataURL={props.palette?.imageUrl || "/images/placeholder.png"}
+          blurDataURL={props.palette?.imageUrl}
           className="rounded"
         />
+
+        <div className="absolute bottom-0 self-center w-full p-4 px-6 text-center rounded-b bg-cod-gray-500 shadow-border-shiny">
+          <p className="text-xl font-semibold">{props?.palette?.title}</p>
+          <p className="text-sm text-silver-900 font-extralight">
+            Generated at 24:53 Monday
+          </p>
+        </div>
       </div>
 
       <div className="flex flex-col justify-between w-full h-full gap-2">
@@ -84,13 +57,6 @@ function Home(props: ImagePreviewProps) {
             ))}
           </div>
         )}
-
-        <div className="self-center p-3 px-6 text-center rounded bg-cod-gray-500 shadow-border-shiny">
-          <p className="text-xl font-semibold">{props?.palette?.title}</p>
-          <p className="text-sm text-silver-900 font-extralight">
-            Generated at 24:53 Monday{" "}
-          </p>
-        </div>
 
         <div className="flex items-center self-end justify-end w-full gap-2">
           <button
@@ -116,8 +82,8 @@ function Home(props: ImagePreviewProps) {
           </button>
         </div>
       </div>
-    </div>
-  );
+    </motion.div>
+  ) : null;
 }
 
 function Export(props: ImagePreviewProps) {
@@ -170,5 +136,56 @@ function Download(props: ImagePreviewProps) {
         <button onClick={props.exported.download}>Download</button>
       </div>
     </div>
+  );
+}
+
+function ColorBox(
+  props: {
+    color: string;
+    index: number;
+  } & CreateController
+) {
+  const isValidHex = CH.isValidHexCode(props.color);
+
+  return isValidHex ? (
+    <motion.div
+      className="flex flex-col items-center justify-center p-2 rounded bg-cod-gray-500 shadow-border-shiny"
+      style={{
+        ...props.colors.animation.getRandomTransformOrigin(),
+      }}
+      custom={props.index}
+      variants={props.colors.animation.variants}
+      animate={props.colors.animation.controls}
+    >
+      <div
+        className="flex items-start justify-between w-full h-20 text-[8px] font-bold transition-all rounded opacity-75 hover:opacity-100 shadow-border-shiny"
+        style={{ backgroundColor: props.color }}
+      >
+        {props.editing.isEditing ? (
+          <>
+            <button className="flex items-center justify-center w-full transition-colors rounded-full bg-cod-gray-500 hover:bg-cod-gray-400">
+              <Edit size={16} strokeWidth={1.22} />
+            </button>
+            <button className="flex items-center justify-center w-full text-center transition-colors rounded-full bg-cod-gray-500 hover:bg-cod-gray-400">
+              <Trash size={16} strokeWidth={1.22} />
+            </button>
+          </>
+        ) : null}
+      </div>
+
+      <motion.span
+        className="mt-1 text-sm font-semibold cursor-pointer text-silver"
+        onClick={() => {
+          copy(props.color);
+        }}
+        whileTap={{ scale: 0.95, color: props.color }}
+      >
+        {props.color}
+      </motion.span>
+    </motion.div>
+  ) : (
+    <button className="flex items-center justify-center w-full transition-all border border-dashed rounded opacity-75 hover:opacity-100 aspect-square shadow-border-shin border-cod-gray-50 h-[120px]">
+      <PlusCircle size={24} strokeWidth={1.22} />
+    </button>
   );
 }
