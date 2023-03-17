4beb80bbb6c6248a0a16630ca6fb119b1d7d88f1
KXLAA <kolade.afode@yahoo.com>
2023-03-17 07:47:56 +0000

    fix: refactor saved palettes controller
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- components/saved/SavedPallettes.tsx
+++ components/saved/SavedPallettes.tsx
@@ -1,4 +1,5 @@
 import type { palettes } from "@prisma/client";
+import { pipe } from "fp-ts/function";
 import { AnimatePresence, motion } from "framer-motion";
 import { BoxSelect, XCircle } from "lucide-react";
 import Image from "next/image";
@@ -13,6 +14,7 @@ import { useSavedPallettes } from "@/lib/hooks/use-saved-pallettes";
 
 export type SavedPalettes = palettes & {
   savedAt: Date;
+  gradient?: string;
 };
 
 type SavedPallettesProps = {
@@ -21,20 +23,51 @@ type SavedPallettesProps = {
 
 export function SavedPallettes(props: SavedPallettesProps) {
   const controller = useSavedPalettes(props.type);
-  const palettes = controller.isRecent
-    ? controller?.palettes?.recent
-    : controller?.palettes?.saved;
 
   return (
     <AnimatePresence>
-      {controller?.palettes?.saved?.length ? (
+      {controller.palettes.isEmpty ? (
+        <Card
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: 20, opacity: 0 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          hidden={controller.isRecent}
+        >
+          <div className="flex flex-col items-center justify-center gap-4">
+            <div className="grid grid-cols-4">
+              {Array.from({ length: 8 }).map((_, i) => (
+                <BoxSelect
+                  key={i}
+                  className={cx(
+                    "w-16 h-16",
+                    i > 3 ? "text-silver-900" : "text-silver-700"
+                  )}
+                  strokeWidth={1}
+                />
+              ))}
+            </div>
+
+            <p className="text-xl font-semibold text-silver-600">
+              You have saved no pallettes yet
+            </p>
+
+            <NextLink
+              href="/create"
+              className="px-4 py-2 text-sm font-semibold text-center transition-colors rounded text-silver-600 bg-cod-gray-500 shadow-border-shiny hover:bg-cod-gray-600"
+            >
+              Create a Pallette
+            </NextLink>
+          </div>
+        </Card>
+      ) : (
         <Card
           dotted
           heading={controller.heading}
           description={controller.description}
           key="saved-pallettes"
           addon={
-            controller.isRecent && controller?.palettes?.saved?.length > 4 ? (
+            controller.isRecent && controller.palettes.saved?.length > 4 ? (
               <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
@@ -51,7 +84,7 @@ export function SavedPallettes(props: SavedPallettesProps) {
           transition={{ duration: 0.5, ease: "easeOut" }}
         >
           <div className="relative grid w-full grid-cols-1 gap-2 md:grid-cols-4">
-            {palettes.map((palette) => (
+            {controller.palettes.items.map((palette) => (
               <motion.div
                 className="relative flex flex-col w-full gap-2"
                 key={palette.id}
@@ -83,9 +116,7 @@ export function SavedPallettes(props: SavedPallettesProps) {
 
                 <GradientBar
                   css={{
-                    $$gradient: `linear-gradient(147deg, ${palette.palette.join(
-                      ", "
-                    )})`,
+                    $$gradient: palette.gradient,
                     height: 16,
                     borderRadius: 2,
                   }}
@@ -100,40 +131,6 @@ export function SavedPallettes(props: SavedPallettesProps) {
             ))}
           </div>
         </Card>
-      ) : (
-        <Card
-          initial={{ y: 20, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: 20, opacity: 0 }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-          hidden={props.type === "recent"}
-        >
-          <div className="flex flex-col items-center justify-center gap-4">
-            <div className="grid grid-cols-4">
-              {Array.from({ length: 8 }).map((_, i) => (
-                <BoxSelect
-                  key={i}
-                  className={cx(
-                    "w-16 h-16",
-                    i > 3 ? "text-silver-900" : "text-silver-700"
-                  )}
-                  strokeWidth={1}
-                />
-              ))}
-            </div>
-
-            <p className="text-xl font-semibold text-silver-600">
-              You have saved no pallettes yet
-            </p>
-
-            <NextLink
-              href="/create"
-              className="px-4 py-2 text-sm font-semibold text-center transition-colors rounded text-silver-600 bg-cod-gray-500 shadow-border-shiny hover:bg-cod-gray-600"
-            >
-              Create a Pallette
-            </NextLink>
-          </div>
-        </Card>
       )}
     </AnimatePresence>
   );
@@ -143,16 +140,21 @@ function useSavedPalettes(type: "recent" | "saved") {
   const [hoverRef, isHovered] = useMouseOver<any>();
   const [saved, setSaved] = useSavedPallettes();
   const isRecent = type === "recent";
-  const heading = isRecent ? "Recently Created" : "Saved Pallettes";
-  const description = isRecent
-    ? "Your recently created pallettes"
-    : "Your saved pallettes";
 
-  function sortSavedPalettes() {
-    return saved.sort(
+  function addGradient(palette: SavedPalettes[]) {
+    return palette.map((p) => ({
+      ...p,
+      gradient: `linear-gradient(147deg, ${p.palette.join(", ")})`,
+    }));
+  }
+
+  function sort(palettes: SavedPalettes[]) {
+    return palettes.sort(
       (a, b) => Number(new Date(b.savedAt)) - Number(new Date(a.savedAt))
     );
   }
+
+  const palettes = pipe(saved, sort, addGradient);
 
   function remove(id: string) {
     setSaved(saved?.filter((p) => p.id !== id));
@@ -163,14 +165,18 @@ function useSavedPalettes(type: "recent" | "saved") {
   }
 
   return {
-    heading,
-    description,
+    heading: isRecent ? "Recently Created" : "Saved Pallettes",
+    description: isRecent
+      ? "Your recently created pallettes"
+      : "Your saved pallettes",
     isRecent,
     hoverRef,
     isHovered,
     palettes: {
-      saved: sortSavedPalettes(),
-      recent: sortSavedPalettes().slice(0, 4),
+      items: isRecent ? palettes.slice(0, 4) : palettes,
+      saved: palettes,
+      recent: palettes.slice(0, 4),
+      isEmpty: palettes.length === 0,
       set: setSaved,
       remove,
       add,
